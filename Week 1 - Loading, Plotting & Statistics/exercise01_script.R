62d743c95ebb123cbfce8e230f1e9c4d6bcee8a9
niklasbaier <68354891+niklasbaier@users.noreply.github.com>
2021-01-05 14:32:26 +0100

    updated exercises

--- Week 1 - Loading, Plotting & Statistics/exercise01_script.R
+++ Week 1 - Loading, Plotting & Statistics/exercise01_script.R
@@ -21,11 +21,11 @@ ncol(df)
 names(df)
 
 # d) List the first rows of the data set.
-head(df, n=20)
+head(df, n = 20)
 
 # e) What is the value range of the attribute - age?
 # the tidyverse way
-summarise(df, min_age=min(age), max_age=max(age))
+summarise(df, min_age = min(age), max_age = max(age))
 
 # the other way
 summary(df$age)
@@ -35,7 +35,7 @@ range(df$age)
 
 # f) Calculate the average of annual hours worked by the laborers with 0, 1, 2, ... 6 kids each
 # the tidyverse way
-df %>% group_by(kids) %>% summarise(mean_lnhr=mean(lnhr))
+df %>% group_by(kids) %>% summarise(mean_lnhr = mean(lnhr))
 
 # the other way
 mean(df[df$kids == 0,]$lnhr)
@@ -48,7 +48,7 @@ mean(df[df$kids == 6,]$lnhr)
 
 # g) Calculate the average number of kids of a 40 year old person
 # the tidyverse way
-df %>% filter(age == 40) %>% summarise(mean_kids=mean(kids))
+df %>% filter(age == 40) %>% summarise(mean_kids = mean(kids))
 
 # the other way
 mean(df[df$age == 40,]$kids)
@@ -58,14 +58,14 @@ mean(df[df$age == 40,]$kids)
 
 # a) Plot a histogram of the attribute age. What is the most frequent age?
 hist(df$age)
-df %>% group_by(age) %>% summarise(count=n()) %>% arrange(desc(count))
+df %>% group_by(age) %>% summarise(count = n()) %>% arrange(desc(count))
 
 # b) Plot the average number of kids against the age and interpret the resulting graph. Underpin your observation using a statistical method
 # the tidyverse way
-plot(df %>% group_by(age) %>% summarise(avg_kids=mean(kids)))
+plot(df %>% group_by(age) %>% summarise(avg_kids = mean(kids)))
 
 # the other way
-plot(aggregate(x=df$kids, by=list(df$age), FUN=mean))
+plot(aggregate(x = df$kids, by = list(df$age), FUN = mean))
 
 cor(df$kids, df$age)
 # the average number of kids decreases with increasing age, the two attributes are correlated negatively
@@ -75,15 +75,15 @@ plot(df$age, df$lnwg)
 
 # d) Plot the mean of the log of hourly wage (lnwg) against the age. How are they correlated? Also compute the correlation between log of hourly wage (lnwg) against the age.
 # the tidyverse way
-plot(df %>% group_by(age) %>% summarise(avg_lnwg=mean(lnwg)))
+plot(df %>% group_by(age) %>% summarise(avg_lnwg = mean(lnwg)))
 
 # the other way
-plot(aggregate(x=df$lnwg, by=list(df$age), FUN=mean))
+plot(aggregate(x = df$lnwg, by = list(df$age), FUN = mean))
 
 cor(df$lnwg, df$age)
 
 # e) Plot lnhr against the age with different colors for disab=0 and disab=1.
-plot(df$age, df$lnhr, pch=df$disab+1, col=c("red", "blue")[df$disab+1])
+plot(df$age, df$lnhr, pch = df$disab + 1, col = c("red", "blue")[df$disab + 1])
 
 # f) Plot a boxplot of the log of annual hours worked (lnhr) against the number of kids. What could be observed regarding mean and variance? Is the observation meaningful for large values of kids?
 boxplot(df$lnhr ~ df$kids)
--- Week 2 - Regression Analytics/exercise02_script.R
+++ Week 2 - Regression Analytics/exercise02_script.R
@@ -7,17 +7,17 @@
 # The consumption per person is measureed in index values, where a high iindex value represents a high consumption.
 
 # previous to tax increase, a
-a <- c(27,31,23,35,26,27,26,18,22,21)
+a <- c(27, 31, 23, 35, 26, 27, 26, 18, 22, 21)
 
 # after tax increase, b
-b <- c(40,36,43,34,25,41,32,29,21,36)
+b <- c(40, 36, 43, 34, 25, 41, 32, 29, 21, 36)
 
 # b) Determine if there is a significant difference in consumption prior to the tax increase and after, utilizing a hypothesis test (significance level alpha = 0.05). The difference is assumed to be normally distributed.
 # paired t-test
-t.test(a,b,alternative = "two.sided", paired=T)
+t.test(a, b, alternative = "two.sided", paired = T)
 
 # <=> test if difference is significantly different from zero
-d <- a-b
+d <- a - b
 t.test(d)
 # H0 is rejected!
 
@@ -25,8 +25,8 @@ t.test(d)
 ## Exercise 2.3 - Statistics
 
 # During a recent study project, a friend of yours asked 8 men and 10 women how many hours per day they wear a mask during the ongoing COVID-19 pandemic.
-female <- c(4,2,3,5,7,2,7,3,5,2)
-male <- c(2,1,5,3,1,3,2,3)
+female <- c(4, 2, 3, 5, 7, 2, 7, 3, 5, 2)
+male <- c(2, 1, 5, 3, 1, 3, 2, 3)
 
 # b) Test the hypothesis "On average, women wear their mask longer per day" with a significance level alpha = 0.05 and 16 degrees of freedom
-t.test(female, male, alternative="greater", paired=F)
+t.test(female, male, alternative = "greater", paired = F)
