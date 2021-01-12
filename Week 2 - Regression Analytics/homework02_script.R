e192a0e570d1bd18c0cf8bc029b71efc85dd247c
niklasbaier <68354891+niklasbaier@users.noreply.github.com>
2021-01-12 10:20:56 +0100

    added homework 2

---
+++ Week 2 - Regression Analytics/homework02_script.R
@@ -0,0 +1,10 @@
+### BUSINESS ANALYTICS ###
+### Homework Sheet 2
+
+
+## Homework 2.5 - Statistics
+
+# a) Assess whether the following sample could possibly have been taken from a population with mean equal to 0 (alpha = 0.05).
+# 2 3 2 4 2 4 5 2 1 4 3 0 3 2 4 5 3 3 0 1
+x <- c(2, 3, 2, 4, 2, 4, 5, 2, 1, 4, 3, 0, 3, 2, 4, 5, 3, 3, 0, 1)
+t.test(x)
